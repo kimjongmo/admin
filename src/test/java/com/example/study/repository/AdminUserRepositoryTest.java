9da832d9c756d0791064316adc9935db7d316f75
kimjongmo <kfmd1008@naver.com>
2019-12-08 21:44:38 +0900

    JpaAudit, 빌더 패턴, 체이닝 메소드 적용

--- src/test/java/com/example/study/repository/AdminUserRepositoryTest.java
+++ src/test/java/com/example/study/repository/AdminUserRepositoryTest.java
@@ -16,16 +16,23 @@ public class AdminUserRepositoryTest extends StudyApplicationTests {
     @Test
     public void create(){
         AdminUser adminUser = new AdminUser();
-        adminUser.setAccount("AdminUser01");
-        adminUser.setPassword("AdminUser01");
+        adminUser.setAccount("AdminUser02");
+        adminUser.setPassword("AdminUser02");
         adminUser.setStatus("REGISTERED");
         adminUser.setRole("PARTNER");
         adminUser.setRegisteredAt(LocalDateTime.now());
-        adminUser.setCreatedAt(LocalDateTime.now());
-        adminUser.setCreatedBy("admin");
+
 
         AdminUser newAdminUser = adminUserRepository.save(adminUser);
 
         Assert.assertNotNull(newAdminUser);
+    }
+
+    @Test
+    public void update(){
+        AdminUser findAdminUser = adminUserRepository.findById(3L).get();
+        findAdminUser.setAccount("AdminUser002");
+
+        adminUserRepository.save(findAdminUser);
     }
 }
