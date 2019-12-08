39ed9b7fff48cd070313eac19dad239674c509d9
kimjongmo <kfmd1008@naver.com>
2019-12-08 23:35:31 +0900

    USER REST API

--- src/main/java/com/example/study/ifs/CrudInterface.java
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -1,4 +1,10 @@
 package com.example.study.ifs;
 
-public interface CrudInterface {
+import com.example.study.model.network.Header;
+
+public interface CrudInterface<Req, Res> {
+    public Header<Res> create(Header<Req> request);
+    public Header<Res> read(Long id);
+    public Header<Res> update(Header<Req> request);
+    public Header delete(Long id);
 }
